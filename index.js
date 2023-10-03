0985d119e4a044f1632103f5ec0cddd3b74988d4
Mohamed Osoble <ossoble360@gmail.com>
2023-10-03 11:37:20 -0400

    magic of functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+   return Math.abs(someValue-42)
+}
+function distanceFromHqInFeet(someValue) {
+   return Math.abs((someValue-42)*264)
+}
+function distanceTravelledInFeet(block1,block2) {
+   return Math.abs((block2 - block1)*264)
+}
+function calculatesFarePrice(start, destination) {
+   let distance = Math.abs((destination - start) * 264);
+ 
+   if (distance <= 400) {
+     return 0;
+   } else if (distance <= 2000) {
+     return ((distance - 400) * 0.02);
+   } else if (distance <= 2500) {
+     return 25;
+   } else {
+     return 'cannot travel that far';
+   }
+ }
